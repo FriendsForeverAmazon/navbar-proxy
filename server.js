4005bd9ddebe30fc6e47c8450a5fb67d7fcbe93b
Joseph Spear <jcspear90@gmail.com>
2019-01-25 17:42:47 -0800

    Added newrelic

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+const newRelic = require('newrelic');
+
 
 app.use(cors());
 app.use('/:id', express.static(path.join(__dirname, 'public')));
